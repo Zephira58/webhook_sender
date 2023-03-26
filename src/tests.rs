033224af10e63a3c25fc18c55fec90b74e92e9f9
Aviion Gibbs <xanthus58@protonmail.com>
2023-03-27 02:40:53 +1030

    Add: More unit tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,9 @@
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+    use self_update::cargo_crate_version;
+    
+
     #[test]
     fn test_get_insult() {
         println!("\nFetching insult from https://insult.mattbas.org/api/insult...");
@@ -24,5 +28,35 @@ mod tests {
             .replace('"', "");
         println!("Affirmation fetched!");
         assert_eq!(x, x);
+    }
+
+    #[test]
+    fn test_send_message() {
+        println!("\nSending message...");
+        let mut request_body = HashMap::new();
+        request_body.insert("content", "unit test");
+        request_body.insert("username", "xanthus");
+        request_body.insert("avatar_url", "https://cdn.discordapp.com/avatars/292971545956188160/2a77b119a3f8ccedfbd513825eec97a5.png?size=1024");
+    
+        reqwest::Client::new()
+            .post("https://discord.com/api/webhooks/1071132108392775720/uYFy-Iyxfi8pO98_PNF3f8fgATHNKzYkSDJ1c4S9_5lvB-axPyTQlF5iHl29cW-6JyPO")
+            .json(&request_body)
+            .send();
+        println!("Message sent!");
+    }
+
+    #[test]
+    pub fn download_update() -> Result<(), Box<dyn (::std::error::Error)>> {
+        let status = self_update::backends::github::Update::configure()
+            .repo_owner("Xanthus58")
+            .repo_name("webhook_sender")
+            .bin_name("github")
+            .show_download_progress(true)
+            .no_confirm(true)
+            .current_version(cargo_crate_version!())
+            .build()?
+            .update()?;
+        println!("Update status: `{}`!", status.version());
+        Ok(())
     }
 }
